a8385fa40f2e4a4f0015d884665d99fc335420ef
Arun Sankars <arun@ceegees.in>
2021-06-09 14:24:23 +0530

    Nolist

--- src/Components/Handler/readLater.handler.js
+++ src/Components/Handler/readLater.handler.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import { get } from 'lodash';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import ReadLaterView from '../Screens/readLaterview';
 
 
 const ReadLaterHandler = () => {
+    const readState = useSelector(state => get(state, 'readLaterData.data'));
+
+    const removeReadLater = (article) => {
+
+    }
+    console.log("datytyy", readState)
   return (
       <ReadLaterView
+        removeReadLater={removeReadLater}
+        allReadLater={readState}
       />
   );
 }
--- src/Components/Screens/readLaterview.js
+++ src/Components/Screens/readLaterview.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import {
-    Grid
+    Grid,
+    Typography
 } from '@material-ui/core';
 import {
     ArrowBack
 } from '@material-ui/icons';
+import { get } from 'lodash';
 
-const ReadLater = () => (
+const ReadLater = ({
+    allReadLater,
+}) => (
     <Grid container className="w3-padding">
         <Grid item xs={12}>
         <a href="/"><ArrowBack />Back to home</a>
         </Grid>
         <Grid item xs={12}>
-            <Grid container>
-                k
-            </Grid>
+        <Grid container>
+            {!get(allReadLater, 'length') ? (
+                 <Grid item xs={12}>ggg</Grid>
+            ) : (
+                <Grid item xs={12} alignItems="center" justify="center" className="w3-padding-64">
+                    <Typography variant="h3" style={{
+                            textAlign: "center"
+                    }} >No Read Later List</Typography>
+                </Grid>
+            )}
+        </Grid>
         </Grid>
     </Grid>
 );
